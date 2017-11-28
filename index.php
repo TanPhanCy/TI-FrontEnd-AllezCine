bad0673f6aadcad9587519b079a982b5125ec4d7
TanPhanCy <phan.tan.brussels@gmail.com>
2017-11-28 09:15:56 +0100

    ajout index

---
+++ index.php
@@ -0,0 +1,378 @@
+
+<!DOCTYPE html>
+<html>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>One Page Layout</title>
+  <body>
+    <header>
+      <div id="top-header">
+        <img src="" alt="" id="header-logo">
+        <input type="search" id="search-box" placeholder="Recherche" /> <button type="submit" name="searcher">Go</button>
+        <div id="info-call-login">
+          <p><i></i>(+000) 123 345 653</p>
+          <button type="button" href="#" name="login-button">LOGIN</button>
+        </div>
+      </div>
+      <div id="bottom-header">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Films
+              <ul style=" display : none;">
+                <li>Actions</li>
+                <li>Adventure</li>
+                <li>Policier</li>
+                <li>...</li>
+              </ul>
+            </li>
+            <li><a href="#">Séries</a>
+              <ul style=" display : none;">
+                <li>Actions</li>
+                <li>Adventure</li>
+                <li>Policier</li>
+                <li>...</li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <div id="main-wrapper">
+      <div class="banner">
+        <div class="banner-description">
+          <button type="button" name="button"><</button>
+          <button type="button" name="button">TARZAN</button>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+          <button type="button" name="button">></button>
+        </div>
+        <div class="banner-proposal">
+          <ul>
+            <li>TARZAN
+            <img src="" class="film-cover">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p></li>
+            <li>Maximum Ride
+            <img src="" class="film-cover">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p></li>
+            <li>Independance
+            <img src="" class="film-cover">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></li>
+            <li>Central Intelligence
+            <img src="" class="film-cover">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></li>
+            <li>Ice Age
+            <img src="" class="film-cover">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p></li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="latest-movies">
+        <div class="thmb">
+          <img src="" class="film-cover-thmb">
+          <p>Titre</p>
+          <p>année</p>
+          <p>Genre</p>
+        </div>
+        <div class="thmb">
+          <img src="" class="film-cover-thmb">
+          <p>Titre</p>
+          <p>année</p>
+          <p>Genre</p>
+        </div>
+        <div class="thmb">
+          <img src="" class="film-cover-thmb">
+          <p>Titre</p>
+          <p>année</p>
+          <p>Genre</p>
+        </div>
+        <div class="thmb">
+          <img src="" class="film-cover-thmb">
+          <p>Titre</p>
+          <p>année</p>
+          <p>Genre</p>
+        </div>
+        <div class="thmb">
+          <img src="" class="film-cover-thmb">
+          <p>Titre</p>
+          <p>année</p>
+          <p>Genre</p>
+        </div>
+      </div>
+
+      <div class="featured-movies">
+        <h2>FEATURED MOVIES</h2>
+        <div class="bread-crum">
+          <a href="#">All</a> <a href="#">Actions</a><a href="#">Policiers</a>
+        </div>
+
+        <div class="featured-movies-list">
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+        <button type="button" name="button">PLUS DE FILMS</button>
+        </div>
+
+        <div class="featured-series-list">
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+          <div class="thmb">
+            <img src="" class="film-cover-thmb">
+            <p>Titre</p>
+            <p>année</p>
+            <p>Genre</p>
+          </div>
+        <button type="button" name="button">PLUS DE FILMS</button>
+        </div>
+
+        <div class="movie-shop">
+          <h2>SHOP MOVIE</h2>
+          <div class="shop-container">
+            <div class="left-big-thmb">
+
+            </div>
+            <div class="right-big-thmb">
+
+            </div>
+            <div class="list-thmb">
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+              <div class="thmb">
+                <img src="" class="film-cover-thmb">
+                <p>Titre</p>
+                <p>année</p>
+                <p>Prix</p>
+              </div>
+            </div>
+
+            <div class="contact-section">
+              <h2>CONTACT US</h2>
+
+              <div class="section" id="adress-section">
+                <i></i>
+                <h3>Address</h3>
+              </div>
+              <div class="section" id="contact-section">
+                <i></i>
+                <h3>Call</h3>
+                <p>Number</p>
+                <p>number</p>
+                <p>number</p>
+              </div>
+              <div class="section" id="mail-section">
+                <i></i>
+                <h3>Address</h3>
+                <p>adresse e-mail</p>
+                <p>adresse e-mail</p>
+                <p>adresse e-mail</p>
+              </div>
+              <div class="section" id="social-media-section">
+                <i></i>
+                <h3>Call</h3>
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Google +</a>
+              </div>
+            </div>
+
+            <div class="contact-form">
+              <form class="" action="treatment.php" method="post">
+                <input type="text" name="" placeholder="FIRST NAME">
+                <input type="text" name="" placeholder="LAST NAME" value="">
+                <input type="text" placeholder="mail" name="" value="">
+                <input type="text" placeholder="subject" name="" value="">
+                <textarea name="name" rows="8" cols="80">YOUR MESSAGE</textarea>
+
+                <button type="submit" name="sender">SEND MESSAGE </button>
+              </form>
+            </div>
+
+            <footer>
+              <div class="top-footer">
+                <input type="search" name="" placeholder="Your email..."><button type="button" name="button">SEND</button>
+                <img src="" alt="logo-footer">
+                <img src="" alt="up-img">
+              </div>
+              <div class="bottom-footer">
+                <p>© 2015 One Movies. All rigths reserved | Design by Becode</p>
+
+                <ul>
+                  <li>Movies</li>
+                  <li>FAQ</li>
+                  <li>Action</li>
+                  <li>Adventure</li>
+                  <li>Comedy</li>
+                  <li>Icons</li>
+                  <li>CONTACT US</li>
+                </ul>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
